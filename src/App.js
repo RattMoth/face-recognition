5be88113536eaf01dbaf8ffdc4f100d4ba95c0e3
Matt <mattanator94@gmail.com>
2019-04-24 15:12:01 -0700

    Imported Rank, FaceRecognition, Particles and added to main App function.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageInputForm from './components/ImageInputForm/ImageInputForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import 'tachyons';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particleStyle} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageInputForm />
-      {/* <FaceRecognition /> */}
+      <FaceRecognition />
     </div>
   );
 }
+
+const particleStyle = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 export default App;
