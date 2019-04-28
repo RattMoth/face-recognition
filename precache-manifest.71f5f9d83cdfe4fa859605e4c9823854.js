7d41c4e5b7c4a4206c240f1c8ffff1ee5bd9103e
Matt <mattanator94@gmail.com>
2019-04-27 19:18:00 -0700

    Updates

---
+++ precache-manifest.71f5f9d83cdfe4fa859605e4c9823854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd89bad1078e9d2071424b2dd565d03c",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "bad11cb44e5dd7fd06a0",
+    "url": "/face-recognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "145148951f9665940528",
+    "url": "/face-recognition/static/css/main.8fa40505.chunk.css"
+  },
+  {
+    "revision": "bad11cb44e5dd7fd06a0",
+    "url": "/face-recognition/static/js/2.d97f9fad.chunk.js"
+  },
+  {
+    "revision": "145148951f9665940528",
+    "url": "/face-recognition/static/js/main.e4dad74a.chunk.js"
+  },
+  {
+    "revision": "2873375fc50fb304d268",
+    "url": "/face-recognition/static/js/runtime~main.4924a73e.js"
+  }
+]);
